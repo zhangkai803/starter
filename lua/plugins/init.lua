78f29dfca908e87399e424001f1650efeda05789
zhangkai803 <kai.zhang.dev@foxmail.com>
2026-01-07 23:44:42 +0800

    relative path to debugger executable

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -104,7 +104,7 @@ return {
     },
     config = function()
       -- Path to the python executable installed by Mason
-      local path = "/Users/k/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+      local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
       require("dap-python").setup(path)
     end,
   },
