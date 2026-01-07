8fb3fd2db13064f414ca72b2098c7a05f19207c9
zhangkai803 <kai.zhang.dev@foxmail.com>
2026-01-07 22:06:54 +0800

    debugger

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -10,3 +10,7 @@ map("i", "jk", "<ESC>")
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 map("n", "<leader>o", "<cmd>AerialToggle! right<CR>", { desc = "Toggle Outline Tree" })
 
+map("n", "<leader>du", "<cmd>lua require'dapui'.toggle() <CR>", { desc = "Toggle DAP UI" })
+map("n", "<leader>db", "<cmd> DapToggleBreakPoint <CR>", { desc = "Toggle Breakpoint" })
+map("n", "<leader>dr", "<cmd> DapContinue <CR>", { desc = "Start/Continue Debugging" })
+
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -62,5 +62,37 @@ return {
         suggestion = { enabled = true, auto_trigger = true },
       })
     end,
-  }
+  },
+  {
+    "mfussenegger/nvim-dap",
+    keys = {
+      { "<leader>db", "<cmd>DapToggleBreakpoint<cr>", desc = "Add breakpoint at line" },
+      { "<leader>dr", "<cmd>DapContinue<cr>", desc = "Start or continue the debugger" },
+    }
+  },
+  {
+    "leoluz/nvim-dap-go",
+    ft = "go",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function()
+      require("dap-go").setup()
+    end,
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+    config = function()
+      local dap, dapui = require("dap"), require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end,
+  },
 }
