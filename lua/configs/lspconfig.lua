f61fed25aebecab2b94d405902217957e1cdb166
zhangkai803 <kai.zhang.dev@foxmail.com>
2026-01-12 16:12:34 +0800

    add js/ts lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "basedpyright", "gopls", "typos_lsp", "rust_analyzer" }
+local servers = { "html", "cssls", "basedpyright", "gopls", "typos_lsp", "rust_analyzer", "vtsls" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
