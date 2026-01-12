fc6cea5298caae576e1ed1f9045cb64ea32652ee
zhangkai803 <kai.zhang.dev@foxmail.com>
2026-01-12 17:36:33 +0800

    fix outline err && style

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,16 +7,16 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
-map("n", "<leader>o", "<cmd> AerialToggle! right <CR>", { desc = "Toggle Outline Tree" })
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd>w <cr>")
+map("n", "<leader>o", "<cmd>AerialToggle right<CR>", { desc = "Toggle Outline Tree" })
 
-map("n", "<leader>du", "<cmd> lua require'dapui'.toggle() <CR>", { desc = "Toggle DAP UI" })
-map("n", "<leader>db", "<cmd> DapToggleBreakpoint <CR>", { desc = "Toggle Breakpoint" })
-map("n", "<leader>dr", "<cmd> DapContinue <CR>", { desc = "Start/Continue Debugging" })
-map("n", "<leader>dc", "<cmd> DapTerminate <CR>", { desc = "Terminate Debugging" })
-map("n", "<leader>ds", "<cmd> DapStepOver <CR>", { desc = "StepOver" })
-map("n", "<leader>di", "<cmd> DapStepInto <CR>", { desc = "StepInto" })
-map("n", "<leader>do", "<cmd> DapStepOut <CR>", { desc = "StepOut" })
+map("n", "<leader>du", "<cmd>lua require'dapui'.toggle()<CR>", { desc = "Toggle DAP UI" })
+map("n", "<leader>db", "<cmd>DapToggleBreakpoint<CR>", { desc = "Toggle Breakpoint" })
+map("n", "<leader>dr", "<cmd>DapContinue<CR>", { desc = "Start/Continue Debugging" })
+map("n", "<leader>dc", "<cmd>DapTerminate<CR>", { desc = "Terminate Debugging" })
+map("n", "<leader>ds", "<cmd>DapStepOver<CR>", { desc = "StepOver" })
+map("n", "<leader>di", "<cmd>DapStepInto<CR>", { desc = "StepInto" })
+map("n", "<leader>do", "<cmd>DapStepOut<CR>", { desc = "StepOut" })
 -- 在 Normal 模式下
 map("n", "<A-j>", ":m .+1<CR>== ", { desc = "Move down" })
 map("n", "<A-k>", ":m .-2<CR>== ", { desc = "Move up" })
@@ -24,5 +24,5 @@ map("n", "<A-k>", ":m .-2<CR>== ", { desc = "Move up" })
 map("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move lines down" })
 map("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move lines up" })
 
-map("n", "<leader>ca", "<cmd> lua vim.lsp.buf.code_action() <CR>", { desc = "Code Action" })
+map("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", { desc = "Code Action" })
 
