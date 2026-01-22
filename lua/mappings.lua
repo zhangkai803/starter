e094ceabf4e63362efa390a7d2222cfbcee68a46
zhangkai803 <kai.zhang.dev@foxmail.com>
2026-01-22 18:44:40 +0800

    Disable marco recording

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,6 +4,7 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
+map("n", "q", "<Nop>", { desc = "Disable Marco Recording" })
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
