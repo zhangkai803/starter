b72e0cbbdd53d42a0c3de9dbe20b2f915e06ed1f
zhangkai803 <kai.zhang.dev@foxmail.com>
2026-04-14 20:39:53 +0800

    add yaml lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "pyright", "gopls", "typos_lsp", "rust_analyzer", "vtsls" }
+local servers = { "html", "cssls", "pyright", "gopls", "typos_lsp", "rust_analyzer", "vtsls", "yaml" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
